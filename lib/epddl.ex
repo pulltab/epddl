09b145cc904fc1c631227798208e44d5a4861721
John Koenig <john@calledshot.org>
2016-01-24 08:57:23 -0800

    Cleanup + module rename.

--- lib/epddl.ex
+++ lib/epddl.ex
@@ -1,2 +1,10 @@
-defmodule Epddl do
+defmodule EPDDL do
+
+  @spec parse(binary) :: term
+  def parse(toParse) do
+    {:ok, tokens} = toParse |> to_char_list |> :pddl.string
+    {:ok, ast} = :ppdl_parser.parse(tokens)
+    ast
+  end
+
 end
--- mix.exs
+++ mix.exs
@@ -1,4 +1,4 @@
-defmodule Epddl.Mixfile do
+defmodule EPDDL.Mixfile do
   use Mix.Project
 
   def project do
