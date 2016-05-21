bcd3b81a5be4f71238f9af14d3338b7e6fef6a7b
John Koenig <john@calledshot.org>
2016-05-21 09:07:18 -0500

    Removed extra whitespace

--- test/probabilistic_test.erl
+++ test/probabilistic_test.erl
@@ -8,4 +8,3 @@ prob_effect_requires_probabilistic_effects_test() ->
     DomainStr = "(define (domain foo) (:action bar :effect (probabilistic 0.5 ())))",
     Domain = epddl:parse(DomainStr),
     ?assert(Domain#domain.requirements == ["probabilistic-effects"]).
-
